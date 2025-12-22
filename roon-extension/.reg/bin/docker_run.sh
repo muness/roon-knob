46fb57684eefad1905e1310b5998691ec9d54647
Muness Castle <931+muness@users.noreply.github.com>
2025-12-22 10:56:34 -0500

    updates to match to extension repo reqs

--- roon-extension/.reg/bin/docker_run.sh
+++ roon-extension/.reg/bin/docker_run.sh
@@ -6,4 +6,4 @@ if [ $? -eq 0 ]; then
     docker rm roon-extension-knob
 fi
 
-docker run -d --network host --restart unless-stopped --name roon-extension-knob -v /Users/muness1/src/roon-extension-generator/out/knob/.reg/etc/config.json:/home/node/config.json muness/roon-extension-knob:latest-arm64v8
+docker run -d --network host --restart unless-stopped --name roon-extension-knob -v roon-knob-data:/home/node/app/data muness/roon-extension-knob:latest
